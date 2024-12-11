2e43af90eb28e562d3726f8a672a1d332afebf34
Puneet Joshi <puneet@openg2p.org>
2024-12-11 06:49:31 +0000

    Env variable issue fixed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Replace placeholders in HTML with environment variables
 envsubst '${SR_ADMIN_URL}' < /usr/share/nginx/html/sr-landing.html > /usr/share/nginx/html/index.html.tmp
-mv /usr/share/nginx/html/index.html.tmp /usr/share/nginx/html/sr-landing.html
+mv /usr/share/nginx/html/index.html.tmp /usr/share/nginx/html/index.html
 
 # Start NGINX
 nginx -g 'daemon off;'
