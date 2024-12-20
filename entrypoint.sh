65ceaedda09825353b31dfa48ada2e685a18c6c0
Lalith Kota <kotalalith@gmail.com>
2024-12-20 15:13:46 +0530

    Removed env vars from list for direct sub
    Signed-off-by: Lalith Kota <kotalalith@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Replace placeholders in HTML with environment variables
-envsubst '${SR_ADMIN_URL} ${SR_REPORTING_URL} ${SR_LOGGING_URL} ${SR_MINIO_URL} ${SR_ODK_URL} ${SR_KAFKA_URL} ${SR_SUPERSET_URL}' < /usr/share/nginx/html/sr-landing.html > /usr/share/nginx/html/index.html.tmp
+envsubst < /usr/share/nginx/html/sr-landing.html > /usr/share/nginx/html/index.html.tmp
 mv /usr/share/nginx/html/index.html.tmp /usr/share/nginx/html/index.html
 
 # Start NGINX
